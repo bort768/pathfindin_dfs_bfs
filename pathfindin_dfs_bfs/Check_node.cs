5b226943593de039f1322e6be9af6abdb2d440e3
bort768 <58220288+bort768@users.noreply.github.com>
2021-11-20 23:52:52 +0100

    dfs zrobiony i interfejs konsolowy

---
+++ pathfindin_dfs_bfs/Check_node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pathfindin_dfs_bfs
+{
+    public class Check_node
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool Visted_node { get; set; }
+        public string Direction { get; set; }
+
+
+    }
+
+    public static class Helper_driectrion
+    {
+        public const string UP = "UP";
+        public const string DOWN = "DOWN";
+        public const string LEFT = "LEFT";
+        public const string RIGHT = "RIGHT";
+    }
+}
